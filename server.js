1bf4023399e4da757204893a031e075ca02cb729
Huzaifa Khalid <huzaifa_khalid97@outlook.com>
2022-04-22 00:53:48 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-
-
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hello from week one assignment</h1>");
+app.use(express.json())
+
+const pokemons = [
+  {
+    id: 1,
+    name: "Pikachu",
+    type: "electric ⚡️",
+    level: 99,
+    image: "/pikachu.webp"
+  }
+]
+
+app.get("/api/pokemons", (req, res) => {
+  res.send({pokemons: pokemons})
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
+app.post("/api/pokemons", (req, res) => {
+  const data = req.body
+  data.id = pokemons.length+1
+  pokemons.push(data)
+  res.send(data)
+})
 
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`listening on port ${port}`))
